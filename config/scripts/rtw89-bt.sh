47b2fad25d5f66938e07c489bfeb146293333379
atonal440 <david@dthl.me>
2023-12-17 02:02:10 -0500

    Hardcode kernel version to current bazzite kernel

--- config/scripts/rtw89-bt.sh
+++ config/scripts/rtw89-bt.sh
@@ -3,6 +3,9 @@ set -oue pipefail
 echo 'cloning module source repo'
 git clone https://github.com/lwfinger/rtw89-BT
 cd rtw89-BT
+export KVER="6.6.3-200.fc39.x86_64"
+echo 'Build for bazzite kernel'
+echo $KVER
 make
 sudo make install
 echo 'This module is NOT SIGNED, secure boot will fail'
